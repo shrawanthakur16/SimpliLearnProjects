aefb3cc8f80b3ad2e0a371d176a56df177cd9c66
shrawanthakur16 <54633307+shrawanthakur16@users.noreply.github.com>
2022-12-25 19:00:51 +0530

    Create Main.java class file
    This class contains main method

---
+++ src/com/LockedMe/Main.java
@@ -0,0 +1,14 @@
+package LockedMe;    //replace with your local package name
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("*************************************\n***  Welcome to LockedMe");
+		System.out.println("***  by Shrawan Kumar Thakur\n*************************************\n\n\n");
+		//FileHandling.createMainFolderIfNotPresent("SimpliLearn");
+		
+		MainMenuOperations.handleWelcomeScreenInput();
+	}
+
+	
+}
